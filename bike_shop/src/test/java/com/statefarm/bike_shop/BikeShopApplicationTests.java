922896c7343a9a3977332ee2c9388e9b40546c6e
tod-easton <tod.easton.fv6o@statefarm.com>
2021-08-06 12:35:09 -0700

    initial push

---
+++ bike_shop/src/test/java/com/statefarm/bike_shop/BikeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.statefarm.bike_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
